a55172fbb62c7d0a43548d828715d41ade6628f2
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-05-25 22:04:26 -0600

    change bio background to boat photo, add animations to header-paragraph and ::after class

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,20 @@
 //     console.log
 //     x.classList.toggle("change");
 // }
+
+// typewriter effect function for header-paragraph
+window.onload = function() {
+  const text = document.getElementById("header-paragraph");
+  text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  const letters = document.querySelectorAll(".letter");
+  letters.forEach((letter, i) => {
+    letter.style.setProperty('--i', i);
+  })
+};
+
+// Stops the pop-up text animation and delay
+document.getElementById("header-paragraph").addEventListener("mouseover", function() {
+  this.classList.add('hovered');
+});
+
+
